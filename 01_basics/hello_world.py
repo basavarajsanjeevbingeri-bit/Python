a9daaa2e5a88f3d466bd2b5354793e6b21943c69
basavarajsanjeevbingeri-bit <basavarajsanjeevbingeri@gmail.com>
2026-06-28 17:19:32 +0530

    Add: Hello World basic program

---
+++ 01_basics/hello_world.py
@@ -0,0 +1,19 @@
+"""
+Hello World Program
+This is the first program to learn Python basics.
+"""
+
+# Single line comment
+print("Hello, World!")
+
+# Print with multiple arguments
+print("Welcome to Python Programming!")
+
+# Print multiple lines
+print("Line 1")
+print("Line 2")
+print("Line 3")
+
+# Print with formatting
+name = "Python"
+print(f"I am learning {name}")
